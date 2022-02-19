6aa8f626f1e8805dc07d560cfa0dae28622cbea7
Charushila1994 <charushilapawar94@gmail.com>
2022-02-19 17:53:22 +0530

    reverse string

---
+++ src/abc/abcdefghi.java
@@ -0,0 +1,13 @@
+package abc;
+
+public class abcdefghi {
+    
+	public static void main(String[] args) 
+	{
+		String num=new String ("my name is charushila pawar");
+		for(int i=num.length()-1;i>=0;i--)
+		{
+			System.out.println(num.charAt(i));
+		}
+	}
+}

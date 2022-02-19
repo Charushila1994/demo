aa10699c5b107c4ee3ef06fa8e1004fe9f746484
Charushila1994 <100039824+Charushila1994@users.noreply.github.com>
2022-02-19 23:40:38 +0530

    Add files via upload

---
+++ swapping.java
@@ -0,0 +1,20 @@
+package myjava;
+
+public class swapping {
+	public static void main(String[] args)
+	{
+		int a=50,b=30, c;
+		/*c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);*/
+		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
